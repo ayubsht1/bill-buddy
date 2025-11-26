8a8851d113adf167bfa660ea1bbfa1ba81bccd7f
Ayub Shrestha <ayubsht1@gmail.com>
2025-11-26 19:29:52 +0545

    full project

--- components/header.jsx
+++ components/header.jsx
@@ -19,13 +19,9 @@ export default function Header() {
     <header className="fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
-          <Image
-            src={"/logos/logo.png"}
-            alt="Vehiql Logo"
-            width={200}
-            height={60}
-            className="h-11 w-auto object-contain"
-          />
+          <h1 className="text-green-600 text-2xl font-bold">
+            Bill Buddy
+          </h1>
         </Link>
 
         {path === "/" && (
